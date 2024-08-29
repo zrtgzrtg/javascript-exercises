5a149596f5827f21e3df8257d0f3ae64c415b35e
zrtg <[thomas4stank@gmail.com]>
2024-08-29 18:40:24 +0200

    Finished exercise with some minor bugfixes
    the callback function of the array.map() method requires a return value
    instead of a traditional assignment. This was the major problem of my
    solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(objArray) {
+    const currentYear = new Date().getFullYear()
+    const yearOfBirthArray = objArray.map(person => person.yearOfBirth)
+    const yearofDeathArray = objArray.map(person => person.yearOfDeath)
+    const yearofDeathArrayFiltered = yearofDeathArray.map(year => {
+        if(year === undefined) {
+            return currentYear
+        } else {
+            return year
+        }
+    })
+    console.log(yearofDeathArray)
+    console.log(yearOfBirthArray)
+    console.log(yearofDeathArrayFiltered)
+    const ageArray = []
+    for(let i = 0; i<yearofDeathArrayFiltered.length;i++) 
+    {
+        ageArray[i] = yearofDeathArrayFiltered[i] - yearOfBirthArray[i]
+    }
+    let oldestPersonAge = Math.max(...ageArray)
+    console.log(ageArray)
+    const oldestIndex = ageArray.findIndex(personAge => personAge === oldestPersonAge)
+    return objArray.at(oldestIndex)
 
 };
 
