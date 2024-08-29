65cd25003ef2baec427ef37183484f468a29aab7
zrtg <[thomas4stank@gmail.com]>
2024-08-29 17:24:05 +0200

    Solved the exercise
    needed to look up the required input types from the tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+  return num1 + num2
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function(numArray) {
+  let sum = 0
+  numArray.forEach(num => sum += num)
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function(numArray) {
+  let res = 1
+  numArray.forEach(num => res = num*res)
+   return res
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+  let ret = 1
+  if(num > 0) {
+      ret = num
+  for(let i = 1;i<power;i++) {
+      ret = ret*num
+  }
+  }
+  if(num < 0) {
+      ret = num
+  for(let i = 0;i<power;i++) {
+      ret = ret/num
+    }
 };
 
-const factorial = function() {
-	
+  return ret
+}
+
+const factorial = function(num) {
+  let ret = 1
+  for(let i = 1;i<=num;i++){
+    ret = ret * i
+  }
+  return ret
 };
 
 // Do not edit below this line
