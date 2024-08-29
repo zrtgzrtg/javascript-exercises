74fe4ff94c0215e1b5336440ba1cf2d6986ba5f7
zrtg <[thomas4stank@gmail.com]>
2024-08-29 17:57:23 +0200

    Solved recursively easily but had minor problem
    the problem again was the type("0")=== String check. the typeof function
    of primitives doesnt return the String object but the "string" string

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(stringNum) {
+    let num = 0
+    if(typeof(stringNum) === "string"){
+        num = parseInt(stringNum)
+    } else {
+        num = stringNum
+    }
+    if(num<0 ){
+        return "OOPS"
+    }
+    if(num === 0) {
+        return 0
+    }
+    if(num === 1) {
+        return 1
+    }
+    return fibonacci(num-1)+fibonacci(num-2)
 
 };
 
